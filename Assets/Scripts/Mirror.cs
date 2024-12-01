6d4289a4c1b45a24a6e5e0efedb643a034d41c03
Guillaume SCHNEIDER <guillaumeschneider@siggraph.org>
2024-12-01 01:08:18 +0100

    add: mirror orientation by right click

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -6,16 +6,34 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Mirror : Entity
 {
+    public static readonly Vector2[] Directions = new Vector2[]
+    {
+        new Vector2(0, 1),        // North
+        new Vector2(0.707f, 0.707f), // Northeast
+        new Vector2(1, 0),        // East
+        new Vector2(0.707f, -0.707f), // Southeast
+        new Vector2(0, -1),       // South
+        new Vector2(-0.707f, -0.707f), // Southwest
+        new Vector2(-1, 0),       // West
+        new Vector2(-0.707f, 0.707f)  // Northwest
+    };
     public Laser laser;
     public Vector2 Orientation;
+    public int currentDirectionIndex = 0;
     public void Awake()
     {
         laser = GetComponentInChildren<Laser>();
+        Orientation = Directions[currentDirectionIndex];
     }
     public void Update()
     {
         if (isGenerating)
         {
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                currentDirectionIndex = (currentDirectionIndex + 1) % Directions.Length;
+                Orientation = Directions[currentDirectionIndex];
+            }
             GetComponent<BoxCollider2D>().enabled = false;
             laser.Cast(transform.position, Orientation);
             GetComponent<BoxCollider2D>().enabled = true;
